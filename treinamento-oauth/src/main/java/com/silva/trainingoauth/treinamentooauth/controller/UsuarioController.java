7d0a4f91a8c0ed555bb20b58f12650d7450ca4de
josue.silva <josue.silva>
2021-03-03 13:01:53 -0300

    att

--- treinamento-oauth/src/main/java/com/silva/trainingoauth/treinamentooauth/controller/UsuarioController.java
+++ treinamento-oauth/src/main/java/com/silva/trainingoauth/treinamentooauth/controller/UsuarioController.java
@@ -3,12 +3,12 @@ package com.silva.trainingoauth.treinamentooauth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.silva.trainingoauth.treinamentooauth.model.dtos.UsuarioDTO;
 import com.silva.trainingoauth.treinamentooauth.service.UsuarioService;
 
 @RestController
@@ -19,9 +19,9 @@ public class UsuarioController {
 	private UsuarioService service;
 	
 	@GetMapping(value = "/buscar-email")
-	public ResponseEntity<UsuarioDTO> findByEmail(@RequestParam String email) {
+	public ResponseEntity<UserDetails> findByEmail(@RequestParam String email) {
 		try {
-			return ResponseEntity.ok(service.findByEmail(email));
+			return ResponseEntity.ok(service.loadUserByUsername(email));
 		}
 		catch (IllegalArgumentException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
--- treinamento-usuario/src/main/java/com/silva/trainingusuario/treinamentousuario/TreinamentoUsuarioApplication.java
+++ treinamento-usuario/src/main/java/com/silva/trainingusuario/treinamentousuario/TreinamentoUsuarioApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class TreinamentoUsuarioApplication implements CommandLineRunner {
 
 	
@@ -18,4 +26,10 @@ public class TreinamentoUsuarioApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.silva.trainingusuario.treinamentousuario.controller"))
+				.paths(PathSelectors.any()).build();
+	}
 }
