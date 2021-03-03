8ff4ace2bb4b4f5b0380c261e8da154ac9de652d
Josué Silva <57103349+silvajosue@users.noreply.github.com>
2021-03-03 13:18:37 -0300

    Update LoginApplication.java

--- treinamento-login/src/main/java/com/silva/training/login/LoginApplication.java
+++ treinamento-login/src/main/java/com/silva/training/login/LoginApplication.java
@@ -6,10 +6,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
 public class LoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.silva.training.controller"))
+				.paths(PathSelectors.any()).build();
 	}
 
 }
